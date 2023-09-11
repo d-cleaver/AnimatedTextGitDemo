7aed13b95c3d2c26b9c8077291f6134dc554afb4
Dan Cleaver <dnlcleaver@gmail.com>
2023-09-11 17:59:18 -0400

    restrict color to purples

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function randomRGB() {
     const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
+    const g = 0;
     const b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`
 }
@@ -13,4 +13,4 @@ const intervalId = setInterval(function () {
     for (let letter of letters) {
         letter.style.color = randomRGB();
     }
-}, 2000);
+}, 1000);
